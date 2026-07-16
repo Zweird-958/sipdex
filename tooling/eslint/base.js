bc9ebd5e632cd8e5cbc875e32f3c9d4003a129e8
zweird <zweir.dd@gmail.com>
2026-07-16 15:51:31 +0200

    fix(eslint): use defineConfig

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -1,10 +1,11 @@
 /// <reference types="./types.d.ts" />
 import eslint from "@eslint/js"
 import turboPlugin from "eslint-plugin-turbo"
+import { defineConfig } from "eslint/config"
 import globals from "globals"
 import tseslint from "typescript-eslint"
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ["**/*.config.*"],
   },
